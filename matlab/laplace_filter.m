2ca8a9450a0ecf4808346e05d00db69ab92b142e
Marc Lasch <mlasch@mailbox.org>
2016-03-10 13:57:13 +0200

    Added noise to signal

--- matlab/laplace_filter.m
+++ matlab/laplace_filter.m
@@ -1,20 +1,29 @@
 fs = 8000;  %sample rate 8khz
 Ts = 1/fs;
 
-l = 16000;   %number of samples
+
+l = 2*fs;   %number of samples 2*fs = 2s
+
 t = (0:l-1)*Ts;   %timebase
 
 f1 = 300;
 f2 = 400;
+
 f3 = 500;
+
 
 A1 = 3;
 A2 = 2;
 A3 = 1.1;
 
+% generate the signal
+
 signal = A1*sin(2*pi*f1*t) ...
     + A2*sin(2*pi*f2*t) ...
     + A3*sin(2*pi*f3*t);
+
+
+signal = awgn(signal,10);   % add noise
 
 %stem(t,signal);
 
